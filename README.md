2c3ec9160174b6e856c2bab15e060c1897f94f19
Jeff H <jeffrey92@gmail.com>
2022-05-11 22:17:56 -0700

    add cpe (circuit playground express) pin defs

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Personal edits for Adafruit [FLORAbrella](https://learn.adafruit.com/florabrella
 ## notes on serial
 
 - [advanced serial linux](https://learn.adafruit.com/welcome-to-circuitpython/advanced-serial-console-on-linux)
+
+## Adafruit Circuit Playground
+
+- [product link](https://www.adafruit.com/product/3333)
+- [sample code](https://github.com/adafruit/Adafruit_CircuitPlayground)
---
+++ lib/include/CPlayExpress.h
@@ -0,0 +1,22 @@
+#ifndef CPLAY_EXPRESS_H
+#define CPLAY_EXPRESS_H
+
+// samd21 definitions from:
+// https://github.com/adafruit/Adafruit_CircuitPlayground/blob/master/Adafruit_Circuit_Playground.h
+
+#define CPLAY_LEFTBUTTON 4        ///< left button pin
+#define CPLAY_RIGHTBUTTON 5       ///< right button pin
+#define CPLAY_SLIDESWITCHPIN 7    ///< slide switch pin
+#define CPLAY_NEOPIXELPIN 8       ///< neopixel pin
+#define CPLAY_REDLED 13           ///< red led pin
+#define CPLAY_IR_EMITTER 25       ///< IR emmitter pin
+#define CPLAY_IR_RECEIVER 26      ///< IR receiver pin
+#define CPLAY_BUZZER A0           ///< buzzer pin
+#define CPLAY_LIGHTSENSOR A8      ///< light sensor pin
+#define CPLAY_THERMISTORPIN A9    ///< thermistor pin
+#define CPLAY_SOUNDSENSOR A4      ///< TBD I2S
+#define CPLAY_LIS3DH_CS -1        ///< LIS3DH chip select pin
+#define CPLAY_LIS3DH_INTERRUPT 27 ///< LIS3DH interrupt pin
+#define CPLAY_LIS3DH_ADDRESS 0x19 ///< LIS3DH I2C address
+
+#endif
