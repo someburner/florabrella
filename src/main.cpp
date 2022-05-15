bb61e9f9dc0b2b10e4a65e72378d229925d2bf60
Jeff H <jeffrey92@gmail.com>
2022-05-14 23:27:29 -0700

    try effects

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 #include "CPlayExpress.h"
-#include <Adafruit_NeoPixel_ZeroDMA.h>
+// #include <Adafruit_NeoPixel_ZeroDMA.h>
+#include <Adafruit_NeoPixel.h>
 #include "LIS3DH.h"
 
 #define DEFAULT_BRIGHTNESS_ONBOARD 32
 #define FRAMES_PER_SECOND  120
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
-Adafruit_NeoPixel_ZeroDMA strip(CPLAY_NUMPIXELS, A2, NEO_GRB);
+// Adafruit_NeoPixel_ZeroDMA strip(CPLAY_NUMPIXELS, A2, NEO_GRB);
+Adafruit_NeoPixel strip(CPLAY_NUMPIXELS, A2, NEO_GRB);
 
 LIS3DH accel;
 
@@ -36,6 +38,47 @@ void setup(void)
     strip.begin();
     strip.setBrightness(DEFAULT_BRIGHTNESS_ONBOARD);
     strip.show();
+}
+
+#define NUM_LEDS 19
+
+
+// ***************************************
+// ** FastLed/NeoPixel Common Functions **
+// ***************************************
+
+// Apply LED color changes
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+// Set a LED color (not yet visible)
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+// Set all LEDs to a given color and apply it (visible)
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue);
+  }
+  showStrip();
 }
 
 // Input a value 0 to 255 to get a color value.
@@ -76,9 +119,83 @@ static void testAccel(void)
     Serial.println(accel.z_g);
 }
 
+void BouncingColoredBalls(int BallCount, byte colors[][3], boolean continuous)
+{
+    float Gravity = -9.81;
+    int StartHeight = 1;
+
+    float Height[BallCount];
+    float ImpactVelocityStart = sqrt( -2 * Gravity * StartHeight );
+    float ImpactVelocity[BallCount];
+    float TimeSinceLastBounce[BallCount];
+    int   Position[BallCount];
+    long  ClockTimeSinceLastBounce[BallCount];
+    float Dampening[BallCount];
+    boolean ballBouncing[BallCount];
+    boolean ballsStillBouncing = true;
+
+    for (int i = 0 ; i < BallCount ; i++) {
+    ClockTimeSinceLastBounce[i] = millis();
+    Height[i] = StartHeight;
+    Position[i] = 0;
+    ImpactVelocity[i] = ImpactVelocityStart;
+    TimeSinceLastBounce[i] = 0;
+    Dampening[i] = 0.90 - float(i)/pow(BallCount,2);
+    ballBouncing[i]=true;
+    }
+
+    while (ballsStillBouncing) {
+    for (int i = 0 ; i < BallCount ; i++) {
+    TimeSinceLastBounce[i] =  millis() - ClockTimeSinceLastBounce[i];
+    Height[i] = 0.5 * Gravity * pow( TimeSinceLastBounce[i]/1000 , 2.0 ) + ImpactVelocity[i] * TimeSinceLastBounce[i]/1000;
+
+    if ( Height[i] < 0 ) {
+    Height[i] = 0;
+    ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i];
+    ClockTimeSinceLastBounce[i] = millis();
+
+    if ( ImpactVelocity[i] < 0.01 ) {
+    if (continuous) {
+    ImpactVelocity[i] = ImpactVelocityStart;
+    } else {
+    ballBouncing[i]=false;
+    }
+    }
+    }
+    Position[i] = round( Height[i] * (NUM_LEDS - 1) / StartHeight);
+    }
+
+    ballsStillBouncing = false; // assume no balls bouncing
+    for (int i = 0 ; i < BallCount ; i++) {
+    setPixel(Position[i],colors[i][0],colors[i][1],colors[i][2]);
+    if ( ballBouncing[i] ) {
+    ballsStillBouncing = true;
+    }
+    }
+
+    showStrip();
+    setAll(0,0,0);
+    }
+}
+
+void colorWipe(byte red, byte green, byte blue, int SpeedDelay) {
+  for(uint16_t i=0; i<NUM_LEDS; i++) {
+      setPixel(i, red, green, blue);
+      showStrip();
+      delay(SpeedDelay);
+  }
+}
+
 void loop(void)
 {
-    rainbow(20);
+    // rainbow(20);
+    // multiple colored balls
+    // byte colors[3][3] = { {0xff, 0x00, 0x00},
+    //                       {0xff, 0xff, 0xff},
+    //                       {0x00, 0x00, 0xff} };
+    // BouncingColoredBalls(3, colors, false);
+    // colorWipe(0x00,0xff,0x00, 50);
+    // colorWipe(0x00,0x00,0x00, 50);
 }
 
 #if 0
