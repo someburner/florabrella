c70b72d5d578c87ea72698cf9de226d9a54ac797
Jeff H <jeffrey92@gmail.com>
2022-05-17 20:33:55 -0700

    add gradient test loop

---
+++ lib/effects/GradientTest.h
@@ -0,0 +1,105 @@
+#ifndef GRADIENTTEST_EFFECT_H
+#define GRADIENTTEST_EFFECT_H
+
+// ping-pongs colors across edges and fades
+
+#include "Arduino.h"
+extern CRGB leds[NUM_LEDS];
+
+class GradientTest {
+  public:
+    GradientTest(){
+        // fill colorIndex array with random numbers
+        for(int i = 0; i < NUM_LEDS; i++) {
+            colorIndex[i] = random8();
+        }
+    };
+    void run();
+
+  private:
+    void beats2();
+    void changingPalette();
+    void bmp1();
+    void bmp2();
+    void bmp_rainbow();
+    uint8_t test = 0;
+    const uint8_t maxTests = 5;
+    uint8_t paletteIndex = 0;
+    uint8_t colorIndex[NUM_LEDS];
+};
+
+// brown green, kinda meh but shows 2 waves
+void GradientTest::beats2(void)
+{
+    uint8_t beatA = beatsin8(120, 0, 255);
+    uint8_t beatB = beatsin8(80, 0, 255);
+    fill_palette(leds, NUM_LEDS, (beatA+beatB)/2, 10, brownGreen, 128, LINEARBLEND);
+    FastLED.show();
+}
+
+// this one is kinda cool
+void GradientTest::changingPalette(void)
+{
+    // fill_palette(leds, NUM_LEDS, paletteIndex, 255 / NUM_LEDS, myPal, 255, LINEARBLEND);
+    fill_palette(leds, NUM_LEDS, paletteIndex, 2, myPal, 255, LINEARBLEND);
+    FastLED.show();
+    EVERY_N_MILLISECONDS(10) {
+        paletteIndex++;
+    }
+}
+
+// kinda cool actually, needs more variation
+void GradientTest::bmp1(void)
+{
+    // create a sin wave at 30bmp for all LEDS, no time base or phase shift
+    uint8_t sinBeat = beatsin8(30, 0, NUM_LEDS-1, 0, 0);
+    leds[sinBeat] = CRGB::Blue;
+    fadeToBlackBy(leds, NUM_LEDS, 10);
+    FastLED.show();
+}
+
+// sorta nice glowing effect
+void GradientTest::bmp2(void)
+{
+    // create a sin wave with period of 2 seconds (30bmp) to change brightness
+    // limit lowest brightness
+    uint8_t sinBeat = beatsin8(30, DEFAULT_BRIGHTNESS, 255, 0, 0);
+
+    for(int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette(greenblue, colorIndex[i], sinBeat);
+    }
+
+    EVERY_N_MILLISECONDS(5) {
+        for(int i = 0; i < NUM_LEDS; i++) {
+            colorIndex[i]++;
+        }
+    }
+    FastLED.show();
+}
+
+// nice rainbow effect, needs variation
+void GradientTest::bmp_rainbow(void)
+{
+    uint8_t beatA = beatsin8(30, 0, 255);
+    uint8_t beatB = beatsin8(20, 0, 255);
+    // fill_palette(leds, NUM_LEDS, (beatA+beatB)/2, 10, orangePink, 255, LINEARBLEND);
+    fill_rainbow(leds, NUM_LEDS, (beatA+beatB)/2, 8);
+    FastLED.show();
+}
+
+void GradientTest::run(void)
+{
+    EVERY_N_SECONDS(5) {
+        test = (test + 1) % maxTests;
+    }
+
+    switch(test) {
+        case 0: beats2(); break;
+        case 1: changingPalette(); break;
+        case 2: bmp1(); break;
+        case 3: bmp2(); break;
+        case 4: bmp_rainbow(); break;
+    }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "EdgeRotate.h"
 #include "EdgeLoop.h"
 #include "PingPong.h"
+#include "GradientTest.h"
 
 // #define USE_NEOPIXEL_DMA
 
@@ -19,7 +20,6 @@
 #endif
 
 CRGB leds[NUM_LEDS];
-uint8_t colorIndex[NUM_LEDS];
 
 #ifdef USE_NEOPIXEL_DMA
 Adafruit_NeoPixel_ZeroDMA strip(CPLAY_NUMPIXELS, A2, NEO_GRB);
@@ -40,37 +40,6 @@ void setup(void)
     strip.begin();
     strip.setBrightness(DEFAULT_BRIGHTNESS);
 #endif
-
-    // fill colorIndex array with random numbers
-    for(int i = 0; i < NUM_LEDS; i++) {
-        colorIndex[i] = random8();
-    }
-}
-
-/// Move a single led
-void color_chase(uint32_t color, uint8_t wait)
-{
-  FastLED.clear();
-  FastLED.setBrightness(DEFAULT_BRIGHTNESS);
-
-  for(int led_number = 0; led_number < NUM_LEDS; led_number++)
-  {
-    leds[led_number] = color;
-    FastLED.show();
-    delay(wait);
-    leds[led_number] = CRGB::Black; //Will clear the current pixel on the next show()
-  }
-  return;
-}
-
-uint8_t paletteIndex = 0;
-
-void gradient_test()
-{
-    uint8_t beatA = beatsin8(120, 0, 255);
-    uint8_t beatB = beatsin8(80, 0, 255);
-    fill_palette(leds, NUM_LEDS, (beatA+beatB)/2, 10, brownGreen, 128, LINEARBLEND);
-    FastLED.show();
 }
 
 bool isRunning = false;
@@ -115,68 +84,20 @@ void run_pingpong(void)
     while(isRunning) pp.run();
 }
 
+// tests - some good ones in there
+void run_gradienttest(void)
+{
+    isRunning = true;
+    GradientTest gt = GradientTest();
+    while(isRunning) gt.run();
+}
+
 void loop(void)
 {
     // run_dropdownfade();
     // run_pingpong();
     // run_edgeloop();
     // run_edgerotate();
-    // gradient_test();
+    run_gradienttest();
     // run_bloom();
-
-    // color_chase(CRGB::White, 200);
-
-
-
-    // for ( int row_index = 0; row_index < BRANCH_LEN; row_index++) {  // 0...18
-    //   for( int column_index = 0; column_index < BRANCHES; column_index++) { // 0...7
-    //     leds[matrix[row_index][column_index]] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-    //   }
-    //   colorIndex += 6;
-    // }
-
-
-
-#if 0
-    // fill_palette(leds, NUM_LEDS, paletteIndex, 255 / NUM_LEDS, myPal, 255, LINEARBLEND);
-    fill_palette(leds, NUM_LEDS, paletteIndex, 2, myPal, 255, LINEARBLEND);
-    FastLED.show();
-    EVERY_N_MILLISECONDS(10) {
-        paletteIndex++;
-    }
-#endif
-
-#if 0
-    // create a sin wave at 30bmp for all LEDS, no time base or phase shift
-    uint8_t sinBeat = beatsin8(30, 0, NUM_LEDS-1, 0, 0);
-    leds[sinBeat] = CRGB::Blue;
-    fadeToBlackBy(leds, NUM_LEDS, 10);
-    FastLED.show();
-#endif
-
-#if 0
-    // create a sin wave with period of 2 seconds (30bmp) to change brightness
-    // limit lowest brightness
-    uint8_t sinBeat = beatsin8(30, DEFAULT_BRIGHTNESS, 255, 0, 0);
-
-    for(int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette(greenblue, colorIndex[i], sinBeat);
-    }
-
-    EVERY_N_MILLISECONDS(5) {
-        for(int i = 0; i < NUM_LEDS; i++) {
-            colorIndex[i]++;
-        }
-    }
-    FastLED.show();
-#endif
-
-#if 0
-    uint8_t beatA = beatsin8(30, 0, 255);
-    uint8_t beatB = beatsin8(20, 0, 255);
-    // fill_palette(leds, NUM_LEDS, (beatA+beatB)/2, 10, orangePink, 255, LINEARBLEND);
-    fill_rainbow(leds, NUM_LEDS, (beatA+beatB)/2, 8);
-    FastLED.show();
-#endif
-
 }
